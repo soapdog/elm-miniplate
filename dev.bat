d6cac81be618777cf940ccc0f22d8dfedce364db
andre alves garzia <andre@andregarzia.com>
2018-08-28 18:33:27 -0300

    adding time travel debugger to the dev server

--- dev.bat
+++ dev.bat
@@ -2,4 +2,4 @@
 
 echo "Launching dev server (elm-live)"
 copy debug.html .\debug\index.html
-call node_modules\.bin\elm-live src\Main.elm --output=debug\elm.js --dir=debug --path-to-elm=node_modules\.bin\elm --open --pushstate
+call node_modules\.bin\elm-live src\Main.elm --debug --output=debug\elm.js --dir=debug --path-to-elm=node_modules\.bin\elm --open --pushstate
